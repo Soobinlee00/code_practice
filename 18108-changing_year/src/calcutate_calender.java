6d29c587df661695ff91aa3ce8ee4dc518d44c6a
Soobin Lee <96915714+Soobinlee00@users.noreply.github.com>
2022-07-02 22:59:52 +0900

    Add files via upload

---
+++ 18108-changing_year/src/calcutate_calender.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class calcutate_calender{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int year = 0;
+		year = sc.nextInt();
+		
+		System.out.println(year-543);
+	}
+
+}
