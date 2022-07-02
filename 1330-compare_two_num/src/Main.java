93a15dfc437314cba8b29e9604e17685e48007cd
Soobin Lee <96915714+Soobinlee00@users.noreply.github.com>
2022-07-02 23:00:15 +0900

    Add files via upload

---
+++ 1330-compare_two_num/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int A,B;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		if (A == B)
+		
+			System.out.println("==");
+		else if (A>B)
+			System.out.println(">");
+		else
+			System.out.println("<");
+	}
+
+}
